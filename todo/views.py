7aa0aef45f06e811f996676230df8d3c57c9d556
leeej <leeej@ji-in.com>
2022-02-23 20:03:36 +0900

    Veu.js없이 Django로 만들기 예제

--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,25 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView, DeleteView, ListView
+from django.urls import reverse_lazy
+from todo.models import Todo
 
 
 class TodoVueOnlyTV(TemplateView):
     template_name = 'todo/todo_vue_only.html'
+
+
+class TodoCV(CreateView):
+    model = Todo
+    fields = '__all__'
+    template_name = 'todo/todo_form.html'
+    success_url = reverse_lazy('todo:list')
+
+
+class TodoLV(ListView):
+    model = Todo
+    template_name = 'todo/todo_list.html'
+
+
+class TodoDelV(DeleteView):
+    model = Todo
+    template_name = 'todo/todo_confirm_delete.html'
+    success_url = reverse_lazy('todo:list')
